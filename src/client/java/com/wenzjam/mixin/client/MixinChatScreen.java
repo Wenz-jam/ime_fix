76488f7e699dab165c506003ac895c8d8f8dd2d7
Wenz-jam <wjj20040520@163.com>
2024-06-23 21:35:15 +0800

    update to 1.20.6

--- src/client/java/com/wenzjam/mixin/client/MixinChatScreen.java
+++ src/client/java/com/wenzjam/mixin/client/MixinChatScreen.java
@@ -5,15 +5,14 @@ import net.minecraft.client.gui.screen.ChatScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ChatScreen.class)
 public class MixinChatScreen {
     @Inject(at = @At("HEAD"), method = "sendMessage", cancellable = true)
-    private void sendMessage(String chatText, boolean addToHistory, CallbackInfoReturnable<Boolean> cir) {
+    private void sendMessage(String chatText, boolean addToHistory, CallbackInfo ci) {
         if(ImeFixer.in_ime()) {
-            cir.setReturnValue(false);
-            cir.cancel();
+            ci.cancel();
         }
     }
 }
